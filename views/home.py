6168792e3856fbcfc83d4338ef18b86d9bdb76d8
Sanjay Kotabagi <sanjaymkotabagi@gmail.com>
2024-03-21 18:38:36 +0530

    Update home.py

--- views/home.py
+++ views/home.py
@@ -4,9 +4,16 @@ import streamlit as st
 def load_view():
     # if lrobj.login_status != True:
     #     log.load_view(lrobj)
-    st.header("CSUT")
-    st.write("""
-Welcome to our cyber security tool, a one-stop solution for all your cyber security needs. Our tool is designed to provide you with a comprehensive set of features that will help you secure your online presence and protect your sensitive information. 
-\n Our tool includes a graphical user interface (GUI) for Nmap, the popular network exploration and security auditing tool. With our Nmap GUI, you can easily scan your network and identify any vulnerabilities that may be present. 
-\n This powerful tool is a must-have for any cyber security professional or network administrator. In addition to our Nmap GUI, we offer a variety of miscellaneous functions that will help you enhance your security posture. Our password strength checker will analyze the strength of your password and provide feedback on how to make it stronger. Our password generator will create strong, unique passwords for all your accounts, ensuring that your online identity is protected. And our email spam checker will help you identify and avoid potentially harmful emails, keeping your inbox clean and secure. With our cyber security tool, you can rest assured that your online presence is protected. 
-\n Whether you're a business owner, a network administrator, or just a concerned individual, our tool is designed to meet all your security needs. So why wait? Try our tool today and start securing your online presence!""")
+    st.title("URL Scanner")
+    st.write("URL scanner app! Please select the number of URLs you want to scan.")
+    st.write("Feel Free to contrubute in GitHub")
+    st.write("---")
+    st.write("Find me on social media:")
+    st.write('<div class="cfooter">'
+        '<a href="https://twitter.com/SanjayKotabagi1"><img src="https://img.shields.io/twitter/follow/SanjayKotabagi1?style=social"></a>'
+        '&nbsp;&nbsp;&nbsp;'  # Inserting spaces between icons
+        '<a href="https://www.linkedin.com/in/sanjaykotabagi/"><img src="https://img.shields.io/badge/LinkedIn-Connect-blue"></a>'
+        '&nbsp;&nbsp;&nbsp;'  # Inserting spaces between icons
+        '<a href="https://github.com/SanjayKotabagi"><img src="https://img.shields.io/github/followers/SanjayKotabagi?label=Follow&style=social"></a>''</div>',
+        unsafe_allow_html=True
+    )
