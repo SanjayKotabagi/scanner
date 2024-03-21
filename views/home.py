1c278c84a300ee16d628052779cd6d5470b9b264
Sanjay Kotabagi <sanjaymkotabagi@gmail.com>
2024-03-21 18:43:14 +0530

    Update home.py

--- views/home.py
+++ views/home.py
@@ -10,8 +10,8 @@ def load_view():
     st.write("---")
     st.write("Find me on social media:")
     st.write('<div class="cfooter">'
-        '<a href="https://twitter.com/SanjayKotabagi1"><img src="https://img.shields.io/twitter/follow/SanjayKotabagi1?style=social"></a>'
-        '&nbsp;&nbsp;&nbsp;'  # Inserting spaces between icons
+        #'<a href="https://twitter.com/SanjayKotabagi1"><img src="https://img.shields.io/twitter/follow/SanjayKotabagi1?style=social"></a>'
+        #'&nbsp;&nbsp;&nbsp;'  # Inserting spaces between icons
         '<a href="https://www.linkedin.com/in/sanjaykotabagi/"><img src="https://img.shields.io/badge/LinkedIn-Connect-blue"></a>'
         '&nbsp;&nbsp;&nbsp;'  # Inserting spaces between icons
         '<a href="https://github.com/SanjayKotabagi"><img src="https://img.shields.io/github/followers/SanjayKotabagi?label=Follow&style=social"></a>''</div>',
